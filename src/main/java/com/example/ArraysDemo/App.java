45f0b5dcbba3cf17cd2c28653af46a27cb861107
marialm0895 <marialm_95@hotmail.com>
2024-05-30 01:08:20 +0200

    declaracion de un array de enteros

--- src/main/java/com/example/ArraysDemo/App.java
+++ src/main/java/com/example/ArraysDemo/App.java
@@ -8,6 +8,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       // ¿cómo se declara un array?
+    	
+    	int[] numeros; // Declaramos que numeros es un array de valores enteros
+    	
+    	// ¿cómo ñadir valores al array de numeros?
+    	//PRIMERAMENTE hay que reservar memoria para los elementos del array
+    	
+    	numeros = new int[10];
+    	
+    	numeros[0] = 1;
+    	numeros[1] = 2;
+    	numeros[2] = 3;
+    	
+    	// Y así, hasta el indice 9, porque del cero al nueve hay 10 elementos.
+    	
     }
 }
