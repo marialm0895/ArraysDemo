8441eae883e55c7da6c54d5267e8dc383ffd1d35
marialm0895 <marialm_95@hotmail.com>
2024-05-30 09:46:42 +0200

    array de objetos introdución

--- src/main/java/com/example/ArraysDemo/App.java
+++ src/main/java/com/example/ArraysDemo/App.java
@@ -8,20 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-    	
-    	// VARIANTE2
-       // ¿cómo se declara un array?
-    	// ESTO ES UN ARRAYZ DE ENTEROS
-    	
-    	int[] numeros = {
-    			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
-    			
-    	};
-    	
-    	// Un rray de nomres seria
-    	String[] nombres = { 
-    			"Tamara", "Jessica", "Carlos","Adrian","Maria Lopez",
-    			"Maria Jose", "Rosa", "Pablo", "Ivan", "Marly"
-    	};
+    
     }
 }
